d38b90fbd702c44158660e2c16300fb96c099aea
MNK5490 <maxwell.n.kendall@gmail.com>
2016-04-26 21:55:36 -0400

    Request for help
    Yeah…. smh

--- main.js
+++ main.js
@@ -10,42 +10,13 @@
 var $image = $('<img>');
 var $overlay = $('<div id="overlay"></div>');
 var $caption = $("<p></p>");
-var $imageLocation;
-var $search;
-var $captionLibrary;
+var $imageArray = $(".images li");
+var $arrowLeft= $('<button id="arrowLeft"> < </button>');
+var $arrowRight = $('<button id="arrowRight"> > </button>');
 
 
-//Actually adding the Overlay
-$overlay.append($image);
-$overlay.append($caption);
-$("body").append($overlay);
+//Search Functionality
 
-//Determining when to Add the overlay
-
-$(".images a").click(function(event){
-  event.preventDefault(event);
-  //var imageHref = $(this).attr("href");
-  var imageLocation = "img/";
-  imageLocation += $(this).attr("href").substr(15);
-     console.log(imageLocation);
-  $($image).attr("src", imageLocation);
-  $overlay.show();
-  var captionText = $(this).children("img").attr("alt");
-  $caption.text(captionText);
-});
-//Hide the overlay
-$overlay.click(function() {
-  //hide the overlay
-  $overlay.hide();
-});
-
-//use each loop to iterate through the list of photos
-
-/*$(".search-input").focus(function(){
-    $(this).val('');
-});
-*/
-//Iterate thru picture elements
 
 $("#search").keyup(function(){
 
@@ -53,14 +24,92 @@ $("#search").keyup(function(){
         if ($(this).hasClass("hide")){
             $(this).removeClass("hide");
         }
-        $captionLibrary = $(this).children('img').attr("alt");
+        var $captionLibrary = $(this).children('img').attr("alt");
         $captionLibrary = $captionLibrary.toLowerCase();
-        $search = $('#search').val();
+        var $search = $('#search').val();
         $search = $search.toLowerCase();
         if ($captionLibrary.indexOf($search) === -1){
             $(this).addClass("hide");
-            console.log("$search");
         }
 })//each
+})//keyup
 
-})//kepup
+
+
+//Determining when to Add the overlay
+
+
+
+$(".images li").click(function (event) {
+    var current = $(this);
+  $(this).addClass("current");
+  event.preventDefault(event);
+// 1. Getting Image Location
+  var imageLocation = "img/";
+  imageLocation += $(this).children("a").attr("href").substr(15);
+  $($image).attr("src", imageLocation);
+
+
+
+  //when clicking the buttons!
+
+  $("#arrowRight").click(function() {
+      var nextImage = "img/";
+      var nextImage = $(".current").next().children("a").attr("href").substr(15);
+      $image = $($image).attr("src", nextImage);
+      debugger;
+  });
+
+  $("#arrowLeft").click(function() {
+      var nextImage = "img/";
+      var nextImage = $(".current").next().children("a").attr("href").substr(15);
+      $image = $($image).attr("src", nextImage);
+      debugger;
+  });
+
+
+
+
+
+
+
+// 2. Getting Caption Text
+  var captionText = $(this).children("img").attr("alt");
+  $caption.text(captionText);
+
+
+
+
+
+// 3. Show Overlay
+ $overlay.show();
+});//end of images li click event
+
+//Hiding the Overlay
+$overlay.click(function() {
+  $overlay.hide();
+});
+
+//Adding the Overlay to the DOM
+$overlay.append($image);
+$overlay.append($caption);
+$overlay.append($arrowRight);
+$overlay.append($arrowLeft);
+$("body").append($overlay);
+
+/*
+
+What needs to be done in order to get the arrows working right:
+
+First:
+
+A. We need to make use of the "index()" method when calling our image to the overlay. This way we can determine:
+
+(1) which item we are on currently
+(2) Which item is behind us
+(3) which item is ahead of us
+(4) if we are at the end of the array of images
+
+We then need to make use of a flag and the ++ and -- operators to go to either the next or the previous sibling element according to the index intelligence
+
+*/
